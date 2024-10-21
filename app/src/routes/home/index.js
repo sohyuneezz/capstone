93b39ddf5e8c193abd5a54f0b021eb5e1a91bd7e
sohyuneezz <csh8838@gmail.com>
2024-10-21 14:16:53 +0900

    폴더 구조 최적화

---
+++ app/src/routes/home/index.js
@@ -0,0 +1,18 @@
+"use  strict"; // 자바스크립트 파일 만들 때 상단에 이크마 스크립트 문법을 준수하겠다 명시하는 게 좋음
+
+const express = require("express");
+const router = express.Router();
+
+const ctrl = require("./home.ctrl");
+
+// router 는 클라이언트의 요청을 연결해주는 부분
+// "/login" 이 요청, (req, res)~ 부터가 실제 수행하는 부분 = 컨트롤러  
+// router.get("/login", (req, res) => { // 앞에 슬래시(루트) 뺴먹으면 작동 안됨
+//     res.render("home/login");
+// });
+
+router.get("/", ctrl.home);
+router.get("/login", ctrl.login);
+
+module.exports = router;
+ 
