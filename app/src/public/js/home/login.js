52d0e99221b3d4f0825a9b668d0bf433dbfad51e
sohyuneezz <csh8838@gmail.com>
2024-10-23 13:44:57 +0900

    프론트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -23,7 +23,17 @@ function login() {
         },
         body: JSON.stringify(req), //문자열 객체로 저장됨
     })
+    //로그인 인증 기능 
     .then((res) => res.json())
-    .then((res) => console.log(res));
-
+    .then((res) => {
+        if (res.success) { // res 가 트루이면 링크로 이동시키기
+            location.href = "/"; // 이동할 링크 걸기(지금은 루트경로)
+        } else {
+            alert(res.msg); // 실패할 시 서버에서 보내는 메시지 띄움
+        }
+    });
+    // 에러 발생 시 에러 처리
+    .catch((err) => {
+        console.error("로그인 중 에러 발생");
+    });
 }
