e12aca804b654da0074cfe5b30c6e687574a4e62
sohyuneezz <csh8838@gmail.com>
2024-11-07 13:47:21 +0900

    UI 수정

--- app/src/public/js/home/careerMap.js
+++ app/src/public/js/home/careerMap.js
@@ -11,12 +11,38 @@ function initializeKnowledgeMap() {
             { id: 4, type: "skill", title: "React" },
             { id: 5, type: "certificate", title: "AWS Certified Developer" },
             { id: 6, type: "job", title: "클라우드 엔지니어" },
+            { id: 7, type: "job", title: "소프트웨어 개발자" },
+            { id: 8, type: "job", title: "데이터 분석가" },
+            { id: 9, type: "job", title: "사이버 보안 전문가" },
+            { id: 10, type: "job", title: "AI 엔지니어" },
+            { id: 11, type: "job", title: "네트워크 관리자" },
+            { id: 12, type: "skill", title: "Python" },
+            { id: 13, type: "skill", title: "R" },
+            { id: 14, type: "skill", title: "Linux" },
+            { id: 15, type: "skill", title: "TensoFlow Developer" },
+            { id: 16, type: "skill", title: "Bash" },
+            { id: 17, type: "certificate", title: "정보처리기사" },
+            { id: 18, type: "certificate", title: "RADP(데이터분석 전문가)" },
+            { id: 19, type: "certificate", title: "CISSP" },
+            { id: 20, type: "certificate", title: "TensorFlow Developer" },
+            { id: 21, type: "certificate", title: "CCNA" }
         ],
         links: [
             { source: 3, target: 4, relation: "학습 경로" },
             { source: 4, target: 1, relation: "필수 스킬" },
             { source: 1, target: 2, relation: "경력 발전" },
             { source: 5, target: 6, relation: "추천 자격증" },
+            { source: 7, target: 12, relation: "필수 스킬" },
+            { source: 8, target: 13, relation: "필수 스킬" },
+            { source: 9, target: 14, relation: "필수 스킬" },
+            { source: 10, target: 15, relation: "필수 스킬" },
+            { source: 11, target: 16, relation: "필수 스킬" },
+            { source: 12, target: 17, relation: "추천 자격증" },
+            { source: 13, target: 18, relation: "추천 자격증" },
+            { source: 14, target: 19, relation: "추천 자격증" },
+            { source: 15, target: 20, relation: "추천 자격증" },
+            { source: 16, target: 21, relation: "추천 자격증" },
+
         ],
     };
 
@@ -30,8 +56,8 @@ function initializeKnowledgeMap() {
         .attr('height', height);
 
     const simulation = d3.forceSimulation(data.nodes)
-        .force('link', d3.forceLink(data.links).id(d => d.id).distance(150))
-        .force('charge', d3.forceManyBody().strength(-300))
+        .force('link', d3.forceLink(data.links).id(d => d.id).distance(150)) // 거리 감소
+        .force('charge', d3.forceManyBody().strength(-100)) // 노드 추가 후 밀어내는 힘 감소
         .force('center', d3.forceCenter(width / 2, height / 2))
         .on('tick', ticked); // tick 이벤트 연결
 
